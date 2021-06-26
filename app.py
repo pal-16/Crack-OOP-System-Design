8c316bfdbf162636eb23a3596fd999adb22042b4
pal-16 <50794716+pal-16@users.noreply.github.com>
2021-06-27 02:22:41 +0530

    added oop design

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# two decorators, same function
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/prep.html/<string:file_name>')
+def library(file_name):
+    return render_template(file_name+'.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
