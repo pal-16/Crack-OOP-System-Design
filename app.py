263eac41eaef7d113e10bcbe89d0d4452c869998
pal-16 <50794716+pal-16@users.noreply.github.com>
2021-07-21 18:32:12 +0530

    refactored code

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
 app = Flask(__name__)
 
 # two decorators, same function
 @app.route('/')
-@app.route('/index.html')
 def index():
     return render_template('index.html')
 
-@app.route('/prep.html/<string:file_name>')
+@app.route('/prep/<string:file_name>')
 def library(file_name):
     return render_template(file_name+'.html')
-
-
-@app.route('/pdf')
-def pdf():
-    return redirect("https://drive.google.com/file/d/1EywoI8J6ediKWKptSU-VJast6mj1r1vV/view?usp=sharing")
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
