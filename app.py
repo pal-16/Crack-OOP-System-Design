7bfce92325bcba4924a3ef781ccda12789a08116
pal-16 <50794716+pal-16@users.noreply.github.com>
2021-07-21 17:49:56 +0530

    added high level system design

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 # two decorators, same function
@@ -11,6 +11,10 @@ def index():
 def library(file_name):
     return render_template(file_name+'.html')
 
+
+@app.route('/pdf')
+def pdf():
+    return redirect("https://drive.google.com/file/d/1EywoI8J6ediKWKptSU-VJast6mj1r1vV/view?usp=sharing")
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
